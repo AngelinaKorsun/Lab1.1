306206b20aaf99afd7c73417bf66fa079365f6b9
AngelinaKorsun <31704835+AngelinaKorsun@users.noreply.github.com>
2020-03-24 15:23:08 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,110 @@
+package com.company;
+import java.awt.*;
+
+import javax.swing.*;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.title.LegendTitle;
+import org.jfree.data.xy.DefaultXYDataset;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+public class App {
+    public static double m = 0;
+    public static double d = 0;
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame frame = new JFrame("Charts");
+
+                frame.setSize(600, 400);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+
+                XYDataset ds = createDataset();
+
+                JFreeChart chart = ChartFactory.createXYLineChart("Random signal",
+                        "N", "t", ds, PlotOrientation.VERTICAL, true, true,
+                        false);
+
+
+                ChartPanel cp = new ChartPanel(chart);
+                JPanel data = new JPanel();
+
+                JLabel label = new JLabel("Mат. очікування: "+ m +"     Дисперсія: " + d );
+                data.add(label);
+
+                Container pane = frame.getContentPane();
+
+                pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+
+                pane.add(cp);
+                pane.add(data);
+
+            }
+        });
+
+    }
+
+    private static XYSeriesCollection createDataset() {
+        double[] dataN = new double[4500];
+        double[] dataX = new double[4500];
+        long[] time = new long[4500];
+
+        int n = 10;
+        int w = 1500;
+        int nn = 1501;
+        double sum = 0;
+        double a;
+        double f;
+
+        long start, end;
+
+
+        for (int t = 500; t < nn; t++) {
+//        start = 0;
+//        end = 0;
+            start = System.nanoTime();
+            sum = 0;
+            a = (Math.random());
+            f = (Math.random());
+            for (int x = 0; x < n; x++) {
+                sum += a * Math.sin(w + f);
+            }
+            dataN[t-500] = t;
+            dataX[t-500] = sum;
+            end = System.nanoTime();
+            time[t-500] = end - start;
+        }
+
+
+        for(int t = 500; t < nn; t++){
+            m += dataX[t-500];
+        }
+
+        for(int t = 500; t < nn; t++){
+            d += Math.pow(dataX[t-500]+ m, 2);
+            d /=(nn-1);
+        }
+
+
+        XYSeriesCollection ds = new XYSeriesCollection();
+
+
+        XYSeries s1 = new XYSeries("s1");
+
+        for (int i = 0; i < dataN.length; i++) {
+            s1.add(dataN[i], time[i]);
+        }
+
+        ds.addSeries(s1);
+
+        return ds;
+    }
+
+}
